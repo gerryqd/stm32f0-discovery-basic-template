504015da2550406fe00ced0ea9686525bb34d99d
gerryqd <wangfengjn@gmail.com>
2018-01-14 11:17:09 +0800

    Adjust the tab size to 2

--- src/main.c
+++ src/main.c
@@ -2,25 +2,25 @@
 
 void SysTick_Handler(void)
 {
-    static uint16_t tick = 0;
+  static uint16_t tick = 0;
 
-    switch (tick++) {
-        case 100:
-            tick = 0;
-            GPIOC->ODR ^= (1 << 8);
-            break;
-    }
+  switch (tick++) {
+    case 100:
+      tick = 0;
+      GPIOC->ODR ^= (1 << 8);
+      break;
+  }
 }
 
 int main(void)
 {
-    // enable the clock to GPIOC
-    //(RM0091 lists this as IOPCEN, not GPIOCEN)
-    RCC->AHBENR |= RCC_AHBENR_GPIOCEN;
+  // enable the clock to GPIOC
+  //(RM0091 lists this as IOPCEN, not GPIOCEN)
+  RCC->AHBENR |= RCC_AHBENR_GPIOCEN;
 
-    GPIOC->MODER = (1 << 16);
+  GPIOC->MODER = (1 << 16);
 
-    SysTick_Config(SystemCoreClock/100);
+  SysTick_Config(SystemCoreClock/100);
 
-    while(1);
+  while(1);
 }
